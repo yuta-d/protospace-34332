0cc699a8ffe514707d90923454bca2651bd6a9f5
yuta-d <yutaro.d.14@gmail.com>
2021-06-14 20:50:55 +0900

    ログアウト状態でのページ遷移修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
-  before_action :authenticate_user!, excect: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_root_path, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user)
